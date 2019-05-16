cecb2ffc490b09e8fdccc35aefc3550c5890eb2e
Kenyon Bunker <kjbunker817@gmail.com>
2019-05-16 10:53:18 -0700

    Step 10
    Added my part for step 10.

--- src/HelloFromAdrian/Main.java
+++ src/HelloFromAdrian/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static final String ANSI_RED = "\u001B[31m";
 
     public static void main(String[] args) {
+
         System.out.println(ANSI_RED + "Hello, my name is Adrian!");
+        System.out.println("Hello Adrian, my name is Kenyon!");
     }
 }
