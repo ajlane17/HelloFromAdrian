e1d9a06d2f7110f36409e2c0d0f441ed503373c6
Adrian Lane <ajlane17@yahoo.com>
2019-05-15 22:58:02 -0600

    modified console output to display in red

--- src/HelloFromAdrian/Main.java
+++ src/HelloFromAdrian/Main.java
@@ -2,7 +2,9 @@ package HelloFromAdrian;
 
 public class Main {
 
+    public static final String ANSI_RED = "\u001B[31m";
+
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Adrian!");
+        System.out.println(ANSI_RED + "Hello, my name is Adrian!");
     }
 }
