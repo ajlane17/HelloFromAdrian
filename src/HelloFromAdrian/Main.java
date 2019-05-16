0030b69974c27bf730f730eb473073f31862557b
Adrian Lane <ajlane17@yahoo.com>
2019-05-15 22:54:16 -0600

    Added 'hello' display

--- src/HelloFromAdrian/Main.java
+++ src/HelloFromAdrian/Main.java
@@ -3,6 +3,6 @@ package HelloFromAdrian;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello, my name is Adrian!");
     }
 }
